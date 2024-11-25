05d62b5ea656c6ff63b3482c6b13af999e73343a
Kyaw Zin Thant <kyawthantver2@gmail.com>
2024-11-25 23:43:45 +0630

    login form UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "For Each Other",
@@ -29,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="bg-lines antialiased  h-full bg-gradient-to-r from-[#F3F6FF] to-white font-nunito overflow-x-hidden">
         {children}
       </body>
     </html>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,91 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const formSchema = z.object({
+    email: z.string().nonempty({ message: "email is required." }),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters long.",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="flex flex-row h-screen w-full items-center justify-center">
+      <div className="w-[400px]">
+        <Card className="p-12">
+          <div className="space-y-2 mb-4 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-gray-500 text-sm dark:text-gray-400">
+              Please fill in the form to Login to an account
+            </p>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
